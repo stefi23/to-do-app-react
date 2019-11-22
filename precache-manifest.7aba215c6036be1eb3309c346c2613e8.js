58c397cda8eb75a8c4fc95312c4212bb0543cece
stefi23 <stefania.rosca2309@gmail.com>
2019-11-22 13:35:55 +0100

    Updates

---
+++ precache-manifest.7aba215c6036be1eb3309c346c2613e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7007776da783c6493f62969375d9caef",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "6e85d4246c9715c0e66f",
+    "url": "/to-do-app-react/static/css/main.7f2bd67c.chunk.css"
+  },
+  {
+    "revision": "adc3756a5e129200d98a",
+    "url": "/to-do-app-react/static/js/2.dff22585.chunk.js"
+  },
+  {
+    "revision": "6e85d4246c9715c0e66f",
+    "url": "/to-do-app-react/static/js/main.f87da0d8.chunk.js"
+  },
+  {
+    "revision": "7cf8c307b111a30a619a",
+    "url": "/to-do-app-react/static/js/runtime-main.5c3cdd97.js"
+  }
+]);
